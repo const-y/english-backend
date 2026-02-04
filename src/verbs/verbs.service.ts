cfba047026a6cf0b1505a52cbeec723917cb692b
Konstantin Yarish <konstantin.yarish@gmail.com>
2026-02-04 20:56:44 +0300

    Маппинг в dto

--- src/verbs/verbs.service.ts
+++ src/verbs/verbs.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
+import { VerbResponseDto } from './dto/verb-response-dto/verb-response-dto';
 
 @Injectable()
 export class VerbsService {
   constructor(private prisma: PrismaService) {}
 
-  getAll() {
-    return this.prisma.verb.findMany();
+  async getAll(): Promise<VerbResponseDto[]> {
+    const verbs = await this.prisma.verb.findMany();
+
+    return verbs.map((verb) => ({
+      id: String(verb.id),
+      base: verb.base,
+      past: verb.past,
+      pastParticiple: verb.participle,
+      translation: verb.translation,
+    }));
   }
 }
